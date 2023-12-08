1b2c79348e0e175989fcb298770066bb04443ae7
J. <77263040+jarvilehto@users.noreply.github.com>
2023-12-08 17:18:49 +0200

    created Card and list components

---
+++ src/components/TodoCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="todo-card">
+    <h2>{{ title }}</h2>
+    <p>{{ description }}</p>
+    <p>Created at: {{ createdAt }}</p>
+    <p>Completed: {{ completed }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.todo-card {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -7,5 +7,31 @@
 </template>
 
 <script>
-// use axios to call my api to get the data and create a variable to store the data
+import axiosd from "axios";
+import TodoCard from "./TodoCard.vue";
+
+export default {
+  name: "TodoList",
+  components: {
+    TodoCard,
+  },
+
+  data() {
+    return {
+      todos: [],
+    };
+  },
+
+  created() {
+    axiosd
+      .get("http://localhost:3000/todos")
+      .then((response) => {
+        this.todos = response.data;
+        console.log(this.todos);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
 </script>
